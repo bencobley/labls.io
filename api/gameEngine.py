b335c68473aa7dfdaf3547827e6f328a1a079ae4
Dimitri Chamay <dimichamay@gmail.com>
2020-02-09 09:24:43 +0000

    Improvements to api

--- api/gameEngine.py
+++ api/gameEngine.py
@@ -1,4 +1,4 @@
-from Colour import Colour
+import time
 
 
 class GameEngine:
@@ -15,9 +15,14 @@ class GameEngine:
         self.redNumberChoice = 0
         self.blueNumberChoice = 0
         self.currentPhotos = []
+        self.redGuess = []
+        self.blueGuess = []
+        self.redChosen = False
+        self.blueChosen = False
 
 
     def play(self):
+        self.currentState = "LOADING"
         round = 0
         NUMBER_OF_ROUNDS = 3
         while round < NUMBER_OF_ROUNDS:
@@ -25,42 +30,26 @@ class GameEngine:
             if self.redLost or self.blueLost:
                 break
             round += 1
-            self.currentState = "ROUND_RESULT"
+            self.currentState = "RESULT-ROUND"
             # passes through redRoundScore and blueRoundScore to server
         self.returnResults()  # get result and create json thing here to send
-        self.currentState = "GAME_RESULT"
+        self.currentState = "RESULT-GAME"
 
-    def returnResults(self):
-        if self.redLost == True and self.blueLost == True:
-            # return draw to server
-            pass
-        elif self.redLost == True:
-            # red hit a bomb, return blues points and red loses
-            pass
-        elif self.blueLost == True:
-            # same as above, define other method and pass in team
-            pass
-        else:
-            # return scores of both teams
-            pass
+
 
     def playRound(self):
+        self.redChosen = False
+        self.blueChosen = False
         photos = self.generateImages()
         self.currentPhotos = photos
         redPhotos = photos[:4]
         bluePhotos = photos[4:7]
         blackPhoto = photos[-1]
-        self.currentState = "CAPTAIN"
-        # send photos to server
-        # recieve captain choices
-
-        # Send word to server
-        self.currentState = "PLAYER"
-        # receive player choices
-        redGuess = ["2", "4", "7"]
-        blueGuess = ["1", "2", "3"]  # GET THESE FROM THE SERVER
-        self.calculateScore(redGuess, redPhotos, blackPhoto, Colour.RED)
-        self.calculateScore(bluePhotos, bluePhotos, blackPhoto, Colour.BLUE)
+        self.currentState = "BOARD-WORD"
+        while self.currentState == "BOARD-WORD":
+            time.sleep(0.5)
+        self.calculateScore(self.redGuess, redPhotos, blackPhoto, "red")
+        self.calculateScore(self.blueGuess, bluePhotos, blackPhoto, "blue")
 
     def calculateScore(self, guesses, teamphotos, blackphoto, team):
         currentTeam = 0
@@ -74,12 +63,12 @@ class GameEngine:
                     currentTeam += 1
                 else:
                     otherTeam += 1
-        if team == Colour.RED:
+        if team == "red":
             self.redLost = bomb
             self.redRoundScore = currentTeam
             self.redScore += self.redRoundScore
             self.blueScore += otherTeam
-        elif team == Colour.BLUE:
+        elif team == "blue":
             self.blueLost = bomb
             self.redScore += otherTeam
             self.blueRoundScore = currentTeam
@@ -97,8 +86,20 @@ class GameEngine:
         self.blueWordChoice = word
         self.blueNumberChoice = number
 
-    def getStatus(self):
+    def setRedGuesses(self, words):
+        self.redGuesses = words
+
+    def setBlueGuesses(self, words):
+        self.blueGuesses = words
+
+    def getState(self):
         return self.currentState
+
+    def getRedWord(self):
+        return self.redWordChoice
+
+    def getBlueWord(self):
+        return self.blueWordChoice
 
     def returnRedScore(self):
         return self.redScore
@@ -145,3 +146,24 @@ class GameEngine:
         ]
         return imageMap
 
+    def setState(self, state):
+        self.currentState = state
+
+    def blueGuessed(self):
+        self.blueChosen = True
+
+    def redGuessed(self):
+        self.redChosen = True
+
+
+    def getBlueQuantity(self):
+        return self.blueNumberChoice
+
+    def getRedQuantity(self):
+        return self.redNumberChoice
+
+    def getRedRoundScore(self):
+        return self.redRoundScore
+
+    def getBlueRoundScore(self):
+        return self.blueRoundScore
--- api/server.py
+++ api/server.py
@@ -7,10 +7,9 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api', methods=['GET','POST'])
+
+@app.route('/api', methods=['GET', 'POST'])
 def index():
-    user_id = request.args.get('uid')
-    game_id = request.args.get('gid')
     request_type = request.args.get('type')
 
     if request.method == 'POST':
@@ -25,57 +24,79 @@ def index():
             quantity = request.form.get('quantity')
             if team == "blue":
                 game.setBlueChoice(word, quantity)
+                game.setBlueQuantity(quantity)
+                game.blueGuessed()
             else:
                 game.setRedChoice(word, quantity)
+                game.setRedQuantity(quantity)
+                game.redGuessed()
             # TODO: save word to game state, to be returned in status
         elif type == "selections":
-            selectedImageIds = json.loads(request.form.get('selections'))
+            selectedImages = json.loads(request.form.get('selections'))
+            if team == "red":
+                game.setRedGuesses(selectedImages)
+            else:
+                game.setBlueGuesses(selectedImages)
 
 
     else:
+
+        team = request.args.get('team')
+
         if request_type == "status":
-            response = {
-                    "data": {
-                        "type": "status",
-                        "attributes": {
-                            "state": game.getStatus(),
-                            "teams":
-                                [
-                                    { "redScore" : game.returnRedScore()},
-                                    { "blueScore" : game.returnBlueScore()}
-
-                                ]
-
-                        }
-                    }
-
+            attributes = {
+                "attributes": {
+                    "state": game.getState(),
+                    "teams":
+                        [
+                            {"redScore": game.returnRedScore()},
+                            {"redRoundScore": game.returnRedRoundScore},
+                            {"blueScore": game.returnBlueScore()},
+                            {"blueRoundScore": game.returnBlueRoundScore}
+                        ]
                 }
+            }
+
+            if game.getState() == "BOARD-SELECT":
+                if team == "red":
+                    newAttributes = attributes.get("attributes")
+                    newAttributes.update({"word": game.getRedWord, "quantity": game.getRedQuantity})
+                    attributes.update({"attributes": newAttributes})
+                else:
+                    newAttributes = attributes.get("attributes")
+                    newAttributes.update({"word": game.getBlueWord, "quantity": game.getBlueQuantity})
+                    attributes.update({"attributes": newAttributes})
+
+
+            response = {
+             "data": {
+                 "type": "status",
+                "attributes": attributes
+
+             }
+            }
             return json.dumps(response)
+
 
         elif request_type == "images":
             response = {
                 "data": {
-                    "type" : "images",
-                        "attributes" : {
-                            "state" : game.getState(),
-                            "images" : game.returnImageMap()
+                    "type": "images",
+                    "attributes": {
+                        "state": game.getState(),
+                        "images": game.returnImageMap()
 
-                        }
                     }
                 }
+            }
             return json.dumps(response)
-
-
 
     # type=images&gid=<GAME_ID>&uid=<USER_ID>
 
-
-
-
     # return request.args.get("user")
-
 
 
 game = GameEngine()
 
 app.run()
+game.setState("LOADING")
